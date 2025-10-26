0029e8a2525f2ecfbca2b23b5c9426fa802f0828
Roman Kovach <root@DESKTOP-6NN71Q5.>
2025-10-26 20:20:41 +0300

    feat: implemented http endpoint

--- internal/server/api.go
+++ internal/server/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log/slog"
+	"net/http"
 	"time"
 
 	"github.com/nats-io/nats.go"
@@ -198,6 +199,28 @@ func (s *Server) handlerDebezium(msg *nats.Msg) {
 	}
 }
 
+func (s *Server) handlerSearch(resp http.ResponseWriter, req *http.Request) {
+	v, e := req.URL.Query()["q"]
+	if !e || len(v) == 0 {
+		http.Error(resp, "Invalid \"q\" param", 400)
+		return
+	}
+	ret := make([]map[string]any, 0)
+	for _, v := range s.es.Search(v[0]) {
+		tmp := map[string]any{}
+		err := json.Unmarshal([]byte(v), &tmp)
+		if err != nil {
+			http.Error(resp, "Err", 500)
+			return
+
+		}
+		ret = append(ret, tmp)
+	}
+
+	resp.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(resp).Encode(ret)
+}
+
 func (s *Server) setupAPI() {
 	nc := s.nats.Conn
 	js, err := nc.JetStream()
@@ -243,4 +266,19 @@ func (s *Server) setupAPI() {
 	if err != nil {
 		slog.Default().Error("Cannot create subscriber", "err", err)
 	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/search", s.handlerSearch)
+	srv := &http.Server{Addr: "0.0.0.0:80", Handler: mux}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error("Http server closed: %v", "error", err)
+		}
+	}()
+
+	go func() {
+		<-s.ctx.Done()
+		srv.Shutdown(s.ctx)
+	}()
 }
